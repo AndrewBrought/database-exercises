1034c057d0a6ee6f4ae948a7a8503749c451c78f
Andrew Brought <andrewbrought@gmail.com>
2020-07-06 16:18:17 -0500

    Finished walkthrough and played around with different requirements

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE IF NOT EXISTS albums(
      id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-     artist VARCHAR(100) DEFAULT 'NONE',
+     artist VARCHAR(100) DEFAULT 'UNKNOWN',
      name VARCHAR(100) NOT NULL ,
-     release_date INT,
-     sales DECIMAL(4,2),
+     release_date SMALLINT UNSIGNED,
+     sales DECIMAL(11,2),
      genre VARCHAR(100),
      PRIMARY KEY (id)
 );
